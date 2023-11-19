10f8cd8b3288b235d9d50642df3aa4eedf8252eb
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-11-19 16:01:08 +0300

    add: SimpleRouter for statements & dormitory

--- core/urls.py
+++ core/urls.py
@@ -2,13 +2,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
 
-from main.views import StatementApiView, DormitoryApiView
+from main.views import StatementViewSet, DormitoryViewSet
+
+router = routers.SimpleRouter()
+router.register(r'statement', StatementViewSet)
+router.register(r'dormitory', DormitoryViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
     path("users/", include("users.urls"), name="users"),
-    path("api/v1/statementlist/", StatementApiView.as_view()),
-    path("api/v1/dormitorylist/", DormitoryApiView.as_view()),
+    path("api/v1/", include(router.urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
