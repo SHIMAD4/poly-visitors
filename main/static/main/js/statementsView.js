ad87eac0a72b758a609675a821e6ae822de5a35e
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-11-23 18:09:26 +0300

    refactor: rewrote the function of rendering statements

--- main/static/main/js/statementsView.js
+++ main/static/main/js/statementsView.js
@@ -2,78 +2,74 @@ const main = document.querySelector('main');
 const statementContainer = document.querySelector('.statement-container');
 const paginationContainer = document.querySelector('.pagination-container');
 
+const clearStatementContainer = () => {
+    statementContainer.innerHTML = '';
+};
+
+const createListElement = (item) => {
+    const list = document.createElement('ul');
+    list.classList.add('elem', 'w-full', 'h-[60px]', 'flex', 'justify-center', 'items-center', 'text-center', 'hover:bg-none', 'mt-[20px]', 'rounded-[19px]');
+
+    const title = createListElementItem('title', item.title, '25%');
+    const payment = createListElementItem('payment', item.payment ? 'Оплачено' : 'Не оплачено', '25%', item.payment ? 'bg-[#69d92e]' : 'bg-[#d41c1c]');
+    const status = createListElementItem('status', getStatusText(item.status), '25%');
+    const fileLink = createListElementFileLink(item.file);
+    const date = createListElementItem('date', item.date, '25%');
+
+    list.append(title, payment, status, fileLink, date);
+    return list;
+};
+
+const createListElementItem = (key, text, width, additionalClass) => {
+    const item = document.createElement('li');
+    item.classList.add('flex', 'justify-center', 'items-center', `w-[${width}]`, 'h-full', 'border-r-[1px]', 'border-[#3a3a3a]', additionalClass);
+    const span = document.createElement('span');
+    span.innerText = text;
+    item.append(span);
+    return item;
+};
+
+const createListElementFileLink = (file) => {
+    const item = document.createElement('li');
+    item.classList.add('flex', 'justify-center', 'items-center', 'w-[25%]', 'h-full', 'cursor-pointer', 'border-r-[1px]', 'border-[#3a3a3a]');
+    const fileLinkAnchor = document.createElement('a');
+    fileLinkAnchor.href = file;
+    fileLinkAnchor.innerText = 'Download File';
+    fileLinkAnchor.classList.add('w-full', 'h-full', 'cursor-pointer', 'flex', 'justify-center', 'items-center');
+    item.append(fileLinkAnchor);
+    return item;
+};
+
+const getStatusText = (status) => {
+    switch (status) {
+        case 'Отправлено':
+        case 'Принято в работу':
+            return 'Отправлено';
+        case 'Готово':
+            return 'Готово';
+        default:
+            return status;
+    }
+};
+
+const renderData = (data) => {
+    clearStatementContainer();
+
+    data.results.forEach(item => {
+        const list = createListElement(item);
+        statementContainer.append(list);
+    });
+
+    if (data.next || data.previous) {
+        renderPaginationButtons(data.next, data.previous);
+    }
+};
+
 const getData = async (url) => {
     try {
-        statementContainer.innerHTML = ''
-
         const response = await fetch(url);
         const data = await response.json();
-
-        data.results.forEach(item => {
-            const list = document.createElement('ul');
-            list.classList.add('elem', 'w-full', 'h-[60px]', 'flex', 'justify-center', 'items-center', 'text-center', 'hover:bg-none', 'mt-[20px]', 'rounded-[19px]');
-
-            const title = document.createElement('li');
-            const payment = document.createElement('li');
-            const status = document.createElement('li');
-            const date = document.createElement('li');
-            const fileLink = document.createElement('li');
-
-            title.classList.add('flex', 'justify-center', 'items-center', 'w-[25%]', 'h-full', 'border-r-[1px]', 'border-[#3a3a3a]');
-            payment.classList.add('flex', 'justify-center', 'items-center', 'w-[25%]', 'h-full', 'border-r-[1px]', 'border-[#3a3a3a]');
-            status.classList.add('flex', 'justify-center', 'items-center', 'w-[25%]', 'h-full', 'border-r-[1px]', 'border-[#3a3a3a]');
-            fileLink.classList.add('flex', 'justify-center', 'items-center', 'w-[25%]', 'h-full', 'cursor-pointer', 'border-r-[1px]', 'border-[#3a3a3a]')
-            date.classList.add('flex', 'justify-center', 'items-center', 'w-[25%]', 'h-full');
-
-            const titleSpan = document.createElement('span');
-            const paymentSpan = document.createElement('span');
-            const statusSpan = document.createElement('span');
-            const dateSpan = document.createElement('span');
-            const fileLinkAnchor = document.createElement('a');
-
-            titleSpan.innerText = item.title;
-            dateSpan.innerText = item.date;
-            fileLinkAnchor.href = item.file;
-            fileLinkAnchor.innerText = 'Download File';
-            fileLinkAnchor.classList.add('w-full', 'h-full', 'cursor-pointer', 'flex', 'justify-center', 'items-center')
-
-            if (item.payment) {
-                paymentSpan.innerText = 'Оплачено';
-                paymentSpan.classList.add('bg-[#69d92e]', 'px-[20px]', 'py-[7px]', 'rounded-[10px]', 'font-bold');
-            } else {
-                paymentSpan.innerText = 'Не оплачено';
-                paymentSpan.classList.add('bg-[#d41c1c]', 'px-[20px]', 'py-[7px]', 'rounded-[10px]', 'font-bold');
-            }
-
-            if (item.status === 'Отправлено' || item.status === 'Принято в работу') {
-                statusSpan.innerText = item.status;
-                statusSpan.classList.add('bg-[#d3eb1e]', 'px-[20px]', 'py-[7px]', 'rounded-[10px]', 'font-bold');
-            } else if (item.status === 'Готово') {
-                statusSpan.innerText = item.status;
-                statusSpan.classList.add('bg-[#69d92e]', 'px-[20px]', 'py-[7px]', 'rounded-[10px]', 'font-bold');
-            } else {
-                statusSpan.innerText = item.status;
-                statusSpan.classList.add('bg-[#d41c1c]', 'px-[20px]', 'py-[7px]', 'rounded-[10px]', 'font-bold');
-            }
-
-            title.append(titleSpan);
-            payment.append(paymentSpan);
-            status.append(statusSpan);
-            fileLink.append(fileLinkAnchor);
-            date.append(dateSpan);
-
-            list.append(title);
-            list.append(payment);
-            list.append(status);
-            list.append(fileLink);
-            list.append(date);
-
-            statementContainer.append(list);
-        });
-
-        if (data.next || data.previous) {
-            renderPaginationButtons(data.next, data.previous);
-        }
+        renderData(data);
     } catch (error) {
         console.error('Error fetching data:', error);
     }
@@ -92,17 +88,17 @@ const createPaginationButton = (label, link) => {
 
 const renderPaginationButtons = (nextLink, prevLink) => {
     paginationContainer.innerHTML = '';
-    paginationContainer.classList.add('mt-10')
+    paginationContainer.classList.add('mt-10');
 
     if (prevLink) {
         const prevButton = createPaginationButton('Previous', prevLink);
-        prevButton.classList.add('mr-5', 'bg-[#2e2e2e]', 'rounded-[10px]', 'py-[10px]', 'px-[20px]')
+        prevButton.classList.add('mr-5', 'bg-[#2e2e2e]', 'rounded-[10px]', 'py-[10px]', 'px-[20px]');
         paginationContainer.appendChild(prevButton);
     }
 
     if (nextLink) {
         const nextButton = createPaginationButton('Next', nextLink);
-        nextButton.classList.add('mr-5', 'bg-[#2e2e2e]', 'rounded-[10px]', 'py-[10px]', 'px-[20px]')
+        nextButton.classList.add('mr-5', 'bg-[#2e2e2e]', 'rounded-[10px]', 'py-[10px]', 'px-[20px]');
         paginationContainer.appendChild(nextButton);
     }
 };
